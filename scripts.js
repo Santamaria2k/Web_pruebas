c8840e0eb6402e5c5011d4bf5ee29dce9a383edd
Santamaria2k <144940234+Santamaria2k@users.noreply.github.com>
2025-11-08 09:05:13 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -282,3 +282,21 @@ var _galerias_home = new Swiper('.galerias_home.swiper-container', {
 	freeMode: true
 });
 }
+
+
+
+function myFunction() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.getElementById("myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Read more";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less";
+	  moreText.style.display = "inline";
+	}
+  }
